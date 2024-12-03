5ffba7ce84d5cca86b1979349f80d90366e77684
Roga <ryanroga@gmail.com>
2024-12-03 09:25:20 -0400

    astro/vercel
    Forced install of @astrojs/vercel. This may be a breaking change, as the npx install suggests that version 5 of astro was required. As we couldn't install version 5 beta without issues with Tailwind, opted to leave it at version 4. This SHOULD still work. I've used the vercel adapter before in other Astro 4 projects, so I assume this is just a bug that hasn't been addressed.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import vercel from '@astrojs/vercel'
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+  output: 'server',
+  adapter: vercel(),
+  integrations: [tailwind()],
+})
