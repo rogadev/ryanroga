d41b24e99f033ea05b9b65917f8af4601970c2fe
Roga <ryanroga@gmail.com>
2024-12-05 11:45:45 -0400

    fix
    attempting to build as static site to see if we can get away from using vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
-// @ts-check
-// @ts-check
 import { defineConfig } from 'astro/config'
-import vercel from '@astrojs/vercel'
 import tailwind from '@astrojs/tailwind'
 
-// https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
   integrations: [tailwind()],
 })
