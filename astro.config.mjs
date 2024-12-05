3194a2d211f56744700b89831cedc7ee1b53e060
Roga <ryanroga@gmail.com>
2024-12-05 12:13:09 -0400

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-import vercel from '@astrojs/vercel'
+import node from '@astrojs/node'
 
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: vercel(),
+  adapter: node({
+    mode: 'standalone',
+  }),
 })
